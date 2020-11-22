8f740a868124bf689b66f2e583460411cf649670
SpiritCroc <dev@spiritcroc.de>
2020-11-22 14:23:26 +0100

    Allow multiple F-Droid repos handled by the same bot/config

--- bot_update.py
+++ bot_update.py
@@ -35,40 +35,40 @@ with open(rp('bot.yaml')) as fin:
 
 
 # fdroid repo path
-def fp(path):
-    return os.path.expanduser(os.path.join(config['fdroid']['repo'], path))
-
-with open(fp("index-v1.json"), "r") as fin:
-    repo_index = json.load(fin)
+def fp(repo_id, path):
+    return os.path.expanduser(os.path.join(config['fdroid'][repo_id]['repo'], path))
 
 
-def get_version_apk(pkg, versionCode):
+def get_version_apk(repo_index, pkg, versionCode):
     for apk in repo_index['packages'][pkg]:
         if int(apk['versionCode']) == versionCode:
             return apk
     return None
 
-def get_version_name(pkg, versionCode):
-    apk = get_version_apk(pkg, versionCode)
+def get_version_name(repo_index, pkg, versionCode):
+    apk = get_version_apk(repo_index, pkg, versionCode)
     if apk != None:
         return apk['versionName']
     return None
 
-def get_direct_download(pkg, versionCode):
-    apk = get_version_apk(pkg, versionCode)
+def get_direct_download(repo_index, repo_id, pkg, versionCode):
+    apk = get_version_apk(repo_index, pkg, versionCode)
     if apk != None and 'apkName' in apk:
-        return config['fdroid']['repo_url'] + "/" + apk['apkName']
+        return config['fdroid'][repo_id]['repo_url'] + "/" + apk['apkName']
     return None
 
-def last_notified_version(pkg):
+def last_notified_version(repo_id, pkg):
     try:
-        with open(os.path.join(pkg_versions_dir, pkg), "r") as fin:
+        with open(os.path.join(pkg_versions_dir, repo_id, pkg), "r") as fin:
             return int(fin.read())
     except:
         return None
 
-def store_last_notified_version(pkg, versionCode):
-    with open(os.path.join(pkg_versions_dir, pkg), "w") as fout:
+def store_last_notified_version(repo_id, pkg, versionCode):
+    store_dir = os.path.join(pkg_versions_dir, repo_id)
+    if not os.path.exists(store_dir):
+        os.makedirs(store_dir)
+    with open(os.path.join(store_dir, pkg), "w") as fout:
         fout.write(str(versionCode))
 
 async def bot_init():
@@ -84,50 +84,49 @@ async def bot_finish():
     await client.close()
 
 async def bot_update():
-    for app in repo_index["apps"]:
-        pkg = app["packageName"]
-        name = app["name"]
-        versionCode = int(app["suggestedVersionCode"])
+    for repo_id in config["fdroid"]:
+        repo_config = config["fdroid"][repo_id]
+        with open(fp(repo_id, "index-v1.json"), "r") as fin:
+            repo_index = json.load(fin)
 
-        last_notified = last_notified_version(pkg)
-        if last_notified == None:
-            print(f"Never notified for {pkg}, remembering version code {versionCode}")
-            store_last_notified_version(pkg, versionCode)
-            continue
-        if last_notified >= versionCode:
-            if verbose:
-                print(f"{pkg} does not require any notification")
-            continue
-        try:
-            versionName = app["suggestedVersionName"]
-        except:
-            versionName = get_version_name(pkg, versionCode)
-        if versionName == None:
-            print(f"WARN: No version name found for {pkg}, {versionCode}")
-            continue
-        try:
-            changes = app["localized"]["en-US"]["whatsNew"]
-            if changes[-1] == '\n':
-                changes = changes[:-1]
-        except:
-            changes = None
-        versionString = f"{versionName}" if versionName != None else f"{versionCode}"
-        repo_name = config["fdroid"]["repo_name"]
-        repo_url = config["fdroid"]["repo_url"]
+        for app in repo_index["apps"]:
+            pkg = app["packageName"]
+            name = app["name"]
+            versionCode = int(app["suggestedVersionCode"])
 
-        msg = f"[{repo_name}]({repo_url}) updated {name} to version {versionString}."
-        if changes != None:
-            msg += f"\n\nChanges:\n\n{changes}"
+            last_notified = last_notified_version(repo_id, pkg)
+            if last_notified == None:
+                print(f"{repo_id}/{pkg}: New app, remember version code {versionCode}")
+                store_last_notified_version(repo_id, pkg, versionCode)
+                continue
+            if last_notified >= versionCode:
+                if verbose:
+                    print(f"{repo_id}/{pkg} does not require any notification")
+                continue
+            try:
+                versionName = app["suggestedVersionName"]
+            except:
+                versionName = get_version_name(pkg, versionCode)
+            if versionName == None:
+                print(f"WARN: No version name found for {pkg}, {versionCode}")
+                continue
+            try:
+                changes = app["localized"]["en-US"]["whatsNew"]
+                if changes[-1] == '\n':
+                    changes = changes[:-1]
+            except:
+                changes = None
+            versionString = f"{versionName}" if versionName != None else f"{versionCode}"
+            repo_name = config["fdroid"][repo_id]["repo_name"]
+            repo_url = config["fdroid"][repo_id]["repo_url"]
+            repoString = repo_name if repo_url == '' else f"[{repo_name}]({repo_url})"
 
-        #download_url = get_direct_download(pkg, versionCode)
-        #if download_url != None and False:
-        #    # Disabled for now: direct download is discouraged, and looks wrong in this case
-        #    msg += f"\n\nAvailable now from the [{repo_name}]({repo_url}) ([direct download]({download_url}))."
-        #else:
-        #    msg += f"\n\nAvailable now from the [{repo_name}]({repo_url})."
+            msg = f"{repoString} updated {name} to version {versionString}."
+            if changes != None:
+                msg += f"\n\nChanges:\n\n{changes}"
 
-        store_last_notified_version(pkg, versionCode)
-        await notify_update(pkg, msg)
+            store_last_notified_version(repo_id, pkg, versionCode)
+            await notify_update(repo_id, pkg, msg)
 
 async def post_notify(room, msg):
     input(f"Notify {room}: {msg}\nPress enter to confirm")
@@ -139,11 +138,12 @@ async def post_notify(room, msg):
     }
     await client.room_send(room, "m.room.message", content, ignore_unverified_devices=True)
 
-async def notify_update(pkg, msg):
+async def notify_update(repo_id, pkg, msg):
     # "all" is a special "package" name to notify about all packages
+    repo_rooms = config["matrix"]["rooms"][repo_id]
     for notify_id in ["all", pkg]:
-        if notify_id in config["matrix"]["rooms"]:
-            for room in config["matrix"]["rooms"][notify_id]:
+        if notify_id in repo_rooms:
+            for room in repo_rooms[notify_id]:
                 await post_notify(room, msg)
 
 async def main():

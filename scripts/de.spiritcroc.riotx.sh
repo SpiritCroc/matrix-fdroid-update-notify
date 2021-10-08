7b8152cae1a1777cab7cb556d38b6eba4d2110ac
SpiritCroc <dev@spiritcroc.de>
2021-10-08 09:43:24 +0200

    Update SchildiChat script

--- scripts/de.spiritcroc.riotx.sh
+++ scripts/de.spiritcroc.riotx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Link to the upstream changelog
-echo "$msg" | sed 's|\(- Update codebase to \)\(Element \)\(v.*\)|\1[\2\3](https://raw.githubusercontent.com/vector-im/element-android/\3/CHANGES.md)|'
+echo "$msg" \
+    | sed 's|\(- Update codebase to \)\(Element \)\(v.*\)|\1[\2\3](https://raw.githubusercontent.com/vector-im/element-android/\3/CHANGES.md)|' \
+    | sed 's|\(MSC \?\)\([1-9][0-9]*\)|[\1\2](https://github.com/matrix-org/matrix-doc/pull/\2)|'

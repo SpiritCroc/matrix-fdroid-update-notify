311096e3b1e67136aabcc76dbd8dc677901a8c73
SpiritCroc <dev@spiritcroc.de>
2022-08-29 13:26:52 +0200

    SchildiChat "Update codebase to" may also be not in a list

--- scripts/de.spiritcroc.riotx.sh
+++ scripts/de.spiritcroc.riotx.sh
@@ -2,5 +2,5 @@
 
 # Link to the upstream changelog
 echo "$msg" \
-    | sed 's|\(- Update codebase to \)\(Element \)\(v.*\)|\1[\2\3](https://raw.githubusercontent.com/vector-im/element-android/\3/CHANGES.md)|' \
+    | sed 's|\(Update codebase to \)\(Element \)\(v.*\)|\1[\2\3](https://raw.githubusercontent.com/vector-im/element-android/\3/CHANGES.md)|' \
     | sed 's|\(MSC \?\)\([1-9][0-9]*\)|[\1\2](https://github.com/matrix-org/matrix-doc/pull/\2)|'
